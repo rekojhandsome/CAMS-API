bd37c8dd020380c5fab26c9409a5b14a76538a3a
John Paul Pendo <rekojhandsome@gmail.com>
2025-07-16 14:14:36 +0800

    added ARH DTO

---
+++ Models/DTO/AssetRequestHeaderDTO/AssetRequestHeaderModel.cs
@@ -0,0 +1,21 @@
+﻿using CAMS_API.Models.Entities;
+
+namespace CAMS_API.Models.DTO.AssetRequestHeaderDTO
+{
+    public class AssetRequestHeaderModel
+    {
+        public int assetRequestID { get; set; }
+        public DateTime assetRequestDate { get; set; } = DateTime.UtcNow;
+        public int employeeID { get; set; }
+        public string? status { get; set; }
+        public decimal totalAssetValue { get; set; }
+        public bool requiresApproval { get; set; }
+
+        public ICollection<AssetRequestDetail> AssetRequestDetails { get; set; } = new List<AssetRequestDetail>();
+
+        public AssetRequestHeaderModel()
+        {
+            AssetRequestDetails = new List<AssetRequestDetail>();
+        }
+    }
+}
