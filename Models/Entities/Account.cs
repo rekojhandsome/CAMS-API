651f732c672dbaa1442f60278eb55c701962cd25
John Paul Pendo <rekojhandsome@gmail.com>
2025-07-17 22:44:36 +0800

    added Account entity

---
+++ Models/Entities/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace CAMS_API.Models.Entities
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public required string Username { get; set; } = string.Empty;
+        public required string Passwrod { get; set; } = string.Empty;
+    }
+}
