1632e449011d54c097740c8a40afd4341be7e323
John Paul Pendo <rekojhandsome@gmail.com>
2025-09-04 18:52:44 +0800

    Created ard service repo on master

---
+++ CAMS-API.Infrastructure/Services/AssetRequestDetailServiceRepository.cs
@@ -0,0 +1,8 @@
+﻿using CAMS_API.Interface;
+
+namespace CAMS_API.CAMS_API.Infrastructure.Services
+{
+    public class AssetRequestDetailServiceRepository : IAssetRequestDetailServiceRepository
+    {
+    }
+}
