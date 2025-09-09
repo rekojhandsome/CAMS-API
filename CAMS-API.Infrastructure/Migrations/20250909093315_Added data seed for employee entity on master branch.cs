0771a71426f1e9db80c2d70224df8c0af808b72f
John Paul Pendo <rekojhandsome@gmail.com>
2025-09-09 17:34:11 +0800

    performed update-database on master branch

---
+++ CAMS-API.Infrastructure/Migrations/20250909093315_Added data seed for employee entity on master branch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CAMS_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addeddataseedforemployeeentityonmasterbranch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeID", "BirthDate", "ContactNo", "DateCreated", "DateHired", "DepartmentID", "Email", "FirstName", "Gender", "LastName", "MiddleName", "PositionID", "Suffix" },
+                values: new object[] { 4, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1234567890, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "admin@gmail.com", "Admin", "Male", "admin", "A", 1, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "EmployeeID",
+                keyValue: 4);
+        }
+    }
+}
